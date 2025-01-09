d13ebd3a67d6621b418e8737cc38bc195c86b43a
frob2001 <frobalino2001@gmail.com>
2025-01-09 11:07:48 -0500

    Dependencias solucionadas

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Habilitar CORS
 
 @app.route("/")
 def hello():
-    return "¡Hola, mundo desde Flask!"
+    return jsonify({"message": "¡Hola, mundo desde Flask!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
